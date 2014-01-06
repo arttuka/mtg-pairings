72c26bb85f31444865228dd4d25c221a34236ebf
Arttu Kaipiainen <arttu.kaipiainen@tut.fi>
2014-01-06 15:25:17 +0200

    Dependencyt

--- mtg-pairings-wer/project.clj
+++ mtg-pairings-wer/project.clj
@@ -1,7 +1,7 @@
 (defproject mtg-pairings "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojess "0.2"]])
