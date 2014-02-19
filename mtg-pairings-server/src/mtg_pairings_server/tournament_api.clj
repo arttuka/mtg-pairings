9e6b9dd859db8816078bc99349afd6e86064be47
Arttu Kaipiainen <arttu.kaipiainen@tut.fi>
2014-02-19 14:53:12 +0200

    Turhat namespacet pois

--- mtg-pairings-server/src/mtg_pairings_server/tournament_api.clj
+++
@@ -1,8 +0,0 @@
-(ns mtg-pairings-server.tournament-api
-  (:require [mtg-pairings-server.util :refer [parse-date]]
-            [mtg-pairings-server.tournaments :refer :all]
-            [compojure.api.sweet :refer :all]
-            [ring.util.http-response :refer :all]
-            [mtg-pairings-server.schema :refer :all]
-            [schema.core :as s]))
-
