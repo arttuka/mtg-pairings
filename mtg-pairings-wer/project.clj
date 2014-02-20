10e6220ac1857a863b807487f7a2aa2895544f3d
Arttu Kaipiainen <arttu.kaipiainen@tut.fi>
2014-02-20 14:55:27 +0200

    Päivitetty seesaw-dependency, lisätty clojure-watch

--- mtg-pairings-wer/project.clj
+++ mtg-pairings-wer/project.clj
@@ -5,5 +5,6 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clojess "0.3"]
-                 [seesaw "1.4.2"]])
+                 [seesaw "1.4.4"]
+                 [clojure-watch "0.1.9"]])
 
