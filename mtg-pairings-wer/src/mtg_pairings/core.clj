eae582b4495f9bb54355ff17b52600a679b3777e
Mikael Niemela <mikael.niemela@tut.fi>
2014-02-24 15:00:29 +0200

    ei tuu enää poikkeuksia

--- mtg-pairings-wer/src/mtg_pairings/core.clj
+++ mtg-pairings-wer/src/mtg_pairings/core.clj
@@ -11,19 +11,19 @@
 (def server-address  (text ""))
 
 
-(defn select-file []
+(defn select-file [event]
   (text! database-location 
          (choose-file :success-fn (fn [fc file] (.getAbsolutePath file)))))
 
 
 (def database-button (button :text "..."
-                       :listen [:action #(select-file)]))
+                       :listen [:action select-file]))
 
 (defn upload-results []
   ())
 
 (def upload-button (button :text "Upload"
-                     :listen [:action #(upload-results)]))
+                     :listen [:action upload-results]))
  
 (def about-window
   (frame
@@ -31,14 +31,15 @@
     :title "About MTG pairings"
     :size [100 :by 100]
     :content (border-panel 
+               :vgap 5
+               :hgap 5
+               :border ""
                :north "This is the WER database sniffer and result uploader"
-               :south (button :text "Close"))))
+               :south (button :text "Close" :listen [:action (fn [event] (-> about-window hide!))]))))
   
 
 (defn about-handler [event]
-  (-> (about-window)
-    pack!
-    show!))
+  (-> about-window pack! show!))
 
 (defn exit-handler [event]
   (System/exit 0))
