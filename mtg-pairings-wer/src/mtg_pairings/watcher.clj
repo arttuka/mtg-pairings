94b78949053dac5cffcc2b1ec0131fa1ca9471b4
Arttu Kaipiainen <arttu.kaipiainen@tut.fi>
2014-02-25 20:14:17 +0200

    datan hakeminen watcherilta

--- mtg-pairings-wer/src/mtg_pairings/watcher.clj
+++ mtg-pairings-wer/src/mtg_pairings/watcher.clj
@@ -13,66 +13,75 @@
         new-tournaments (remove #((set (keys @state)) (:TournamentId %)) tournaments)
         new-tournaments (into {} (for [tournament new-tournaments]
                                    [(:TournamentId tournament)
-                                    {:name (:Name tournament)
+                                    {:name (:Title tournament)
                                      :rounds (:NumberOfRounds tournament)
-                                     :teams {:uploaded []
-                                             :waiting []}
-                                     :seatings {:uploaded []
-                                                :waiting []}
-                                     :pairings {:uploaded []
-                                                :waiting []}
-                                     :results {:uploaded []
-                                               :waiting []}
+                                     :day (:StartDate tournament)
+                                     :teams []
+                                     :seatings []
+                                     :pairings []
+                                     :results []
                                      :tracking false}]))]
     (swap! state merge new-tournaments)
     (doseq [[id t] new-tournaments]
       (handler id))))
 
-(defn ^:private round-done [results]
+(defn ^:private round-done? [results]
   (not-any? neg? (map :team1_wins results)))
-
-(defn ^:private set-uploaded! [k]
-  (fn [tournament]
-    (let [data (get-in tournament [k :waiting])]
-      (if (seq data)
-        (-> tournament
-          (assoc-in [k :waiting] [])
-          (assoc-in [k :uploaded] data))
-        tournament))))
 
 (defn ^:private tournament-checker [handlers]
   (fn [db tournament-id]
     (println "Checking tournament" tournament-id)
-    (letfn [(handler [type values]
+    (letfn [(handler! [type values]
               (let [f (get handlers type)]
-                (when (and (not= (get-in @state [tournament-id type :uploaded]) values)
-                           (not= (get-in @state [tournament-id type :waiting]) values))
-                  (swap! state assoc-in [tournament-id type :waiting] values)
+                (when (not= (get-in @state [tournament-id type]) values)
+                  (swap! state assoc-in [tournament-id type] values)
                   (when f
                     (f tournament-id)))))]
-      (handler :teams (reader/teams db tournament-id))
-      (handler :seatings (reader/seatings db tournament-id))
-      (handler :pairings (reader/pairings db tournament-id))
-      (handler :results (filter round-done (reader/results db tournament-id))))))
+      (handler! :teams (reader/teams db tournament-id))
+      (handler! :seatings (reader/seatings db tournament-id))
+      (handler! :pairings (reader/pairings db tournament-id))
+      (handler! :results (filter round-done? (reader/results db tournament-id))))))
+
+(defn get-tournament [id]
+  (let [tournament (get @state id)]
+    (select-keys tournament [:name :rounds :day])))
+
+(defn get-pairings [id round]
+  (nth (get-in @state [id :pairings]) (dec round) nil))
+
+(defn get-pairings-count [id]
+  (count (get-in @state [id :pairings])))
+
+(defn get-seatings [id]
+  (seq (get-in @state [id :seatings])))
+
+(defn get-results [id round]
+  (nth (get-in @state [id :results]) (dec round) nil))
+
+(defn get-results-count [id]
+  (count (get-in @state [id :results])))
+
+(defn get-teams [id]
+  (seq (get-in @state [id :teams])))
 
 (defn start! [path & {:as handlers}]
   (let [file (File. path)
         fname (.getName file)
-        checker (tournament-checker handlers)
-        on-change (fn [files]
-                    (doseq [f files
-                            :when (= fname (.getName f))]
-                      (with-open [db (reader/open (.getPath f))]
-                        (check-tournaments! db (:tournament handlers))
-                        (doseq [[id tourn] @state
-                                :when (:tracking tourn)]
-                          (checker db id)))))]
+        checker! (tournament-checker handlers)
+        on-change! (fn [files]
+                     (doseq [f files
+                             :when (= fname (.getName f))]
+                       (with-open [db (reader/open (.getPath f))]
+                         (check-tournaments! db (:tournament handlers))
+                         (doseq [[id tourn] @state
+                                 :when (:tracking tourn)]
+                           (checker! db id)))))]
     (if-not (.exists file)
       (throw (FileNotFoundException. path))
       (reset! watcher (watch/watcher [(.getParent file)]
                         (watch/rate 100)
                         (watch/file-filter (filename fname))
-                        (watch/on-change on-change))))))
+                        (watch/on-change on-change!))))))
 
 (defn stop! []
   (when @watcher
