72f87405dc4d68aa226c7526adaf660cd47900b5
Arttu Kaipiainen <arttu.kaipiainen@tut.fi>
2014-02-27 19:11:19 +0200

    Callbackit optionaalisiksi

--- mtg-pairings-wer/src/mtg_pairings/uploader.clj
+++ mtg-pairings-wer/src/mtg_pairings/uploader.clj
@@ -4,46 +4,45 @@
 
 (defmacro response [callback]
   `(fn [response#]
-     (doto (update-in response# [:body] #(json/parse-string % true))
-       ~callback)))
+     (let [callback# (or ~callback identity)] 
+       (doto (update-in response# [:body] #(json/parse-string % true))
+         callback#))))
 
 (defmacro GET [url callback]
   `(http/get ~url {} (response ~callback)))
 
 (defmacro POST [url options callback]
-  (let [opts (update-in options [:body] json/generate-string)] 
-    `(http/post ~url ~opts (response ~callback))))
+  `(http/post ~url ~options (response ~callback)))
 
 (defmacro PUT [url options callback]
-  (let [opts (update-in options [:body] json/generate-string)] 
-    `(http/put ~url ~opts (response ~callback))))
+  `(http/put ~url ~options (response ~callback)))
 
 (defn options [api-key body]
   {:timeout 1000
    :query-params {:key api-key}
-   :body body})
+   :body (json/generate-string body)})
 
-(defn upload-tournament! [url api-key tournament callback]
+(defn upload-tournament! [url api-key tournament & [callback]]
   (POST (str url "/tournament") 
         (options api-key tournament) 
         callback))
 
-(defn upload-teams! [url tournament-id api-key teams callback]
+(defn upload-teams! [url tournament-id api-key teams & [callback]]
   (PUT (str url "/tournament/" tournament-id "/teams")
        (options api-key teams)
        callback))
 
-(defn upload-seatings! [url tournament-id api-key seatings callback]
+(defn upload-seatings! [url tournament-id api-key seatings & [callback]]
   (PUT (str url "/tournament/" tournament-id "/seatings")
        (options api-key seatings)
        callback))
 
-(defn upload-pairings! [url tournament-id round api-key pairings callback]
+(defn upload-pairings! [url tournament-id round api-key pairings & [callback]]
   (PUT (str url "/tournament/" tournament-id "/round-" round "/pairings") 
        (options api-key pairings)
        callback))
 
-(defn upload-results! [url tournament-id round api-key results callback]
+(defn upload-results! [url tournament-id round api-key results & [callback]]
   (PUT (str url "/tournament/" tournament-id "/round-" round "/results") 
-       (options api-key pairings)
+       (options api-key results)
        callback))
