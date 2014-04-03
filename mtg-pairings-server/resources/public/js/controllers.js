d3570dff399e1191e61e2c71de4911c2bf43cb12
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2014-04-03 15:24:59 +0300

    Turnaussivulla kierrosten järjestys laskevaksi

--- mtg-pairings-server/resources/public/js/controllers.js
+++ mtg-pairings-server/resources/public/js/controllers.js
@@ -32,14 +32,14 @@ angular.module('controllers', [])
 .controller('TournamentController', function($scope, $routeParams, TournamentResource) {
   TournamentResource.get({id: $routeParams.tournament}).$promise.then(function(tournament) {
     $scope.model = tournament;
-    $scope.model.round_nums = _.union(tournament.round, tournament.standings);
+    $scope.model.round_nums = _(_.union(tournament.round, tournament.standings)).sortBy().reverse().value();
   });
 })
 
 .controller('TournamentsController', function($scope, TournamentResource) {
   TournamentResource.query().$promise.then(function(tournaments) {
     $scope.tournaments = _.map(tournaments, function(tournament) {
-      tournament.round_nums = _.union(tournament.round, tournament.standings);
+      tournament.round_nums = _(_.union(tournament.round, tournament.standings)).sortBy().reverse().value();
       return tournament;
     });
   });
@@ -66,7 +66,7 @@ angular.module('controllers', [])
     } else {
       TournamentResource.query().$promise.then(function(tournaments) {
         $scope.tournaments = _.map(tournaments, function(tournament) {
-          tournament.round_nums = _.union(tournament.round, tournament.standings);
+          tournament.round_nums = _(_.union(tournament.round, tournament.standings)).sortBy().reverse().value();
           return tournament;
         });
       });
