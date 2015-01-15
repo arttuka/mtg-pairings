64a47d0b86b389d7a6aad61683d91f78a1405775
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2015-01-15 20:08:39 +0200

    Dictionarylle ja listille get-funktio

---
+++ mtg-pairings-win/MtgPairings/MtgPairings/Service/Util.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using MtgPairings.Functional;
+using System;
+
+namespace MtgPairings.Service
+{
+    public static class Util
+    {
+        public static Option<V> Get<K, V>(this IDictionary<K, V> source, K key)
+        {
+            V value;
+            if (source.TryGetValue(key, out value))
+            {
+                return Option.Of(value);
+            }
+            else
+            {
+                return Option<V>.Empty;
+            }
+        }
+
+        public static Option<T> Get<T>(this IList<T> source, int index)
+        {
+            if (source.Count > index)
+            {
+                return Option.Of(source[index]);
+            }
+            else
+            {
+                return Option<T>.Empty;
+            }
+        }
+    }
+}
---
+++ mtg-pairings-win/MtgPairings/MtgPairingsTest/UtilTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MtgPairings.Service;
+using MtgPairings.Functional;
+
+namespace MtgPairingsTest
+{
+    [TestClass]
+    public class UtilTest
+    {
+        [TestMethod]
+        public void TestDictionaryGet()
+        {
+            IDictionary<int, int> dict = new Dictionary<int, int>() {{1, 1}};
+
+            Assert.AreEqual(dict.Get(1).Value, 1);
+            Assert.IsFalse(dict.Get(0).HasValue);
+        }
+
+        [TestMethod]
+        public void TestListGet()
+        {
+            IList<int> list = new List<int>() {1};
+
+            Assert.AreEqual(list.Get(0).Value, 1);
+            Assert.IsFalse(list.Get(1).HasValue);
+        }
+    }
+}
