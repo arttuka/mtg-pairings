62483547e2ab9f14b7c7b1c7b5454120306da2e2
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2016-04-01 01:12:03 +0300

    Palvelimen versio 1.1

--- mtg-pairings-server/project.clj
+++ mtg-pairings-server/project.clj
@@ -1,4 +1,4 @@
-(defproject mtg-pairings-server "1.0.0"
+(defproject mtg-pairings-server "1.1.0"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.8.0"]
