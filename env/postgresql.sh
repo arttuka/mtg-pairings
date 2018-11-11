49b6de20dc98e8821f19275805a899c430918449
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2018-11-11 15:13:54 +0200

    Vagrantiin uusi Ubuntu ja PostgreSQL

--- env/postgresql.sh
+++ env/postgresql.sh
@@ -2,20 +2,22 @@
 set -eu
 
 # asennetaan postgre
-apt-get install -y postgresql-9.6
+echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list.d/pgdg.list
+
+apt-get update && apt-get install -y postgresql-10
 
 # alustetaan postgre
 service postgresql initdb
 
 # postgre kuuntelemaan ja ottamaan vastaan yhteyksia muualta kuin localhostista
-PG_HBA=/etc/postgresql/9.6/main/pg_hba.conf
+PG_HBA=/etc/postgresql/10/main/pg_hba.conf
 
 echo 'local   all             all                  peer' > "$PG_HBA"
 echo 'host    all             all             all  md5' >> "$PG_HBA"
 
 chown postgres:postgres "$PG_HBA"
 
-echo "listen_addresses = '*'" >> /etc/postgresql/9.6/main/postgresql.conf
+echo "listen_addresses = '*'" >> /etc/postgresql/10/main/postgresql.conf
 
 # kayntiin
 service postgresql start
