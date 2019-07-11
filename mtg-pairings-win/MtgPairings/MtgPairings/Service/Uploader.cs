64b9d1282521bb3de6f36a21ad353a52fe5b650e
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2019-07-11 11:06:10 +0300

    Update to .NET 4.7.2

--- mtg-pairings-win/MtgPairings/MtgPairings/Service/Uploader.cs
+++ mtg-pairings-win/MtgPairings/MtgPairings/Service/Uploader.cs
@@ -60,7 +60,7 @@ namespace MtgPairings.Service
             var request = new RestRequest(method);
             request.Resource = url;
             request.RequestFormat = DataFormat.Json;
-            request.AddBody(body);
+            request.AddJsonBody(body);
             return request;
         }
 
